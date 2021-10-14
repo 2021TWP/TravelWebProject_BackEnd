d188691a66fc0f60cfa81a1ed03c930c94f633dd
hanbikimm <zacquisha130@gmail.com>
2021-10-14 10:18:34 +0900

    cache

--- travelweb/board/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from board.models import Category, Board, Comment
-
-
-admin.site.register(Category)
-admin.site.register(Board)
-admin.site.register(Comment)
--- travelweb/schedule/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from schedule.models import Schedule, Schedule_content
-
-admin.site.register(Schedule)
-admin.site.register(Schedule_content)
